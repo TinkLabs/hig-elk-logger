6065e8ef4f652d0e471260525c88c498a61b834e
cypher.zhong <1992zwm@gmail.com>
2019-04-22 19:39:38 +0800

    stop createConnection

--- services/filebeat-logger.js
+++ services/filebeat-logger.js
@@ -26,10 +26,14 @@ class FilebeatLogger {
         if (!this.enable) {
             return;
         }
-        const client = net.createConnection({
-            port: 13012,
-            host: '127.0.0.1'
-        });
+        // const client = net.createConnection({
+        //     port: 13012,
+        //     host: '127.0.0.1'
+        // }, () => {
+        //     //'connect' listener
+        //     console.log('connected to server!');
+        //     client.write('world!\r\n');
+        // });
 
         const obj = {
             ...this.baseInfo,
